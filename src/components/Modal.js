eda2a585f9819a5f1b5ce002dff54c1de283ca59
AniAzatyan90 <144601535+AniAzatyan90@users.noreply.github.com>
2026-04-18 20:46:39 +0400

    FE-10 crate footer end change lesson descriptor (#8)
    Co-authored-by: David Davtyan <davit.davtyan84@gmail.com>

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,8 +6,11 @@ const Modal = ({ isOpen, onClose, children, width = "400px" }) => {
   return (
     <div className="fixed inset-0 bg-black/40 flex items-center justify-center z-50">
       <div className="bg-white rounded-3xl shadow-2xl p-6 relative"  style={{ width }} >
+       
         <button onClick={onClose}  className="absolute top-4 right-4 text-gray-400 text-xl hover:text-gray-600">
-          ✕
+<h2 className="absolute top-2 right-3 text-red-500 text-xl font-bold z-[9999] cursor-pointer">
+    X
+</h2>
         </button>
         {children}
       </div>
